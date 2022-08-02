7852c49b6d69d26bbb83c9b357f0622fd6f1b37a
Eric Andrechek <eric@andrechek.com>
2022-08-02 14:36:27 -0400

    config building UI complete (without config comments)

--- website/config.js
+++ website/config.js
@@ -11,11 +11,13 @@ ready(function () {
     uploadListener();
     configButtonSetup();
     fetch(
-        'https://raw.githubusercontent.com/EricAndrechek/trackuino-v2/main/software/config.h'
+        'https://raw.githubusercontent.com/EricAndrechek/trackuino-v2/main/software/config.h?rnd=' +
+            Date.now()
     )
         .then((response) => response.text())
         .then((text) => {
             parseConfig(text, true);
+            raw_config = text;
             verifiedSchema = parsedConfigBuffer;
         });
 });
@@ -24,6 +26,7 @@ const debug = true;
 
 let parsedConfigBuffer;
 let verifiedSchema;
+let raw_config;
 
 const uploadListener = () => {
     const fileInput = document.querySelector('#upload input[type=file]');
@@ -36,17 +39,17 @@ const uploadListener = () => {
                 if (parseConfig(fr.result, false)) {
                     document
                         .getElementById('use-config')
-                        .classList.remove('is-hidden');
+                        .classList.remove('hidden');
                     document
                         .getElementById('upload-parse-error')
-                        .classList.add('is-hidden');
+                        .classList.add('hidden');
                 } else {
                     document
                         .getElementById('use-config')
-                        .classList.add('is-hidden');
+                        .classList.add('hidden');
                     document
                         .getElementById('upload-parse-error')
-                        .classList.remove('is-hidden');
+                        .classList.remove('hidden');
                 }
             };
             fr.readAsText(fileInput.files[0]);
@@ -54,9 +57,244 @@ const uploadListener = () => {
     };
 };
 
+const saveConfig = () => {
+    // get the id and value of every input in the config-options id element
+    const configOptions = document.getElementById('config-options');
+    const fields = configOptions.getElementsByClassName('field');
+    const config = {};
+    for (let i = 0; i < fields.length; i++) {
+        const field = fields[i];
+        const input = field.getElementsByTagName('input')[0];
+        const comment = field.getElementsByTagName('p')[0].innerHTML;
+        console.log(input.id);
+        const configKey = input.id.split('{')[1].split('}')[0];
+        const configSubKey = input.id.split('{')[2].split('}')[0];
+        const configSubKeyOption = input.id.split('{')[3].split('}')[0];
+        let configValue;
+        if (input.type === 'checkbox') {
+            configValue = input.checked;
+        } else {
+            configValue = input.value;
+        }
+        if (config[configKey] === undefined) {
+            config[configKey] = {};
+        }
+        if (config[configKey][configSubKey] === undefined) {
+            config[configKey][configSubKey] = {};
+        }
+        config[configKey][configSubKey][configSubKeyOption] = {
+            value: configValue,
+            comment: comment,
+        };
+    }
+
+    let configBuffer = `#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// THIS IS THE TRACKUINO FIRMWARE CONFIGURATION FILE. YOUR CALLSIGN AND
+// OTHER SETTINGS GO HERE.
+//
+// NOTE: all pins are Arduino based, not the Atmega chip. Mapping:
+// https://www.arduino.cc/en/Hacking/PinMapping\n\n\n`;
+    for (const key in config) {
+        configBuffer +=
+            '// --------------------------------------------------------------------------\n';
+        configBuffer += '// ' + key + '\n';
+        configBuffer +=
+            '// --------------------------------------------------------------------------\n\n';
+        for (const subKey in config[key]) {
+            configBuffer += '// ' + subKey + '\n\n';
+            for (const subKeyOption in config[key][subKey]) {
+                let val = config[key][subKey][subKeyOption].value;
+                const comment = config[key][subKey][subKeyOption].comment;
+                if (val === true) {
+                    configBuffer += '#define ' + subKeyOption + '\n';
+                } else if (val === false) {
+                    configBuffer += '// #define ' + subKeyOption + '\n';
+                } else {
+                    // TODO: build type checking logic in to verify type matching
+                    if (isNaN(parseInt(val))) {
+                        val = '"' + val + '"';
+                    }
+                    configBuffer +=
+                        '#define ' + subKeyOption + ' ' + val + '\n';
+                }
+            }
+            configBuffer += '\n';
+        }
+        configBuffer += '\n';
+    }
+    configBuffer += '\n#endif';
+
+    document.getElementById('config-textarea').value = configBuffer;
+    document.getElementById('config-options').classList.add('hidden');
+    document.getElementById('save-config').classList.remove('hidden');
+};
+
+// from https://stackoverflow.com/a/30810322/7974356
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement('textarea');
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = '0';
+    textArea.style.left = '0';
+    textArea.style.position = 'fixed';
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(
+        function () {
+            console.log('Async: Copying to clipboard was successful!');
+        },
+        function (err) {
+            console.error('Async: Could not copy text: ', err);
+        }
+    );
+}
+
+const copyConfig = () => {
+    document.getElementById('config-textarea').focus();
+    document.getElementById('config-textarea').select();
+    copyTextToClipboard(document.getElementById('config-textarea').value);
+    document.getElementById('copy-success').classList.remove('hidden');
+    setTimeout(() => {
+        document.getElementById('copy-success').classList.add('hidden');
+    }, 2000);
+};
+
+const downloadConfig = () => {
+    const configText = document.getElementById('config-textarea').value;
+    const blob = new Blob([configText], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.download = 'config.h';
+    link.href = url;
+    link.click();
+    URL.revokeObjectURL(url);
+};
+
+const toggleSiblings = (elem) => {
+    let sibling = elem.parentNode.firstChild;
+
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== elem) {
+            sibling.classList.toggle('hidden');
+        }
+        sibling = sibling.nextSibling;
+    }
+};
+
 const configSetup = () => {
-    document.getElementById('upload').classList.add('is-hidden');
-    // need to now
+    document.getElementById('upload').classList.add('hidden');
+    console.log(JSON.stringify(parsedConfigBuffer));
+    const hr = document.createElement('hr');
+    document.getElementById('config-options').appendChild(hr);
+    // make input boxes or checkboxes for each config option
+    for (const config in parsedConfigBuffer) {
+        // make new div for config option
+        const configDiv = document.createElement('div');
+        configDiv.classList.add('config-option');
+        // make new label for config option
+        const configLabel = document
+            .getElementById('section-dropdown-template')
+            .cloneNode(true);
+        configLabel.removeAttribute('id');
+        configLabel.getElementsByTagName('span')[0].innerHTML = config;
+        configLabel
+            .getElementsByTagName('span')[0]
+            .classList.add('config-label', 'title', 'is-4');
+        configLabel.getElementsByTagName('span')[0].removeAttribute('id');
+        configDiv.appendChild(configLabel);
+        for (const subConfig in parsedConfigBuffer[config]) {
+            // make new div for sub config option
+            const subConfigDiv = document.createElement('div');
+            subConfigDiv.classList.add('sub-config-option');
+            // make new label for sub config option
+            const subConfigLabel = document.createElement('p');
+            subConfigLabel.classList.add(
+                'sub-config-label',
+                'subtitle',
+                'is-4'
+            );
+            subConfigLabel.innerHTML = subConfig;
+            subConfigDiv.appendChild(subConfigLabel);
+            if (config.includes(' MODULE')) {
+                subConfigDiv.classList.add('hidden');
+            }
+            for (const key in parsedConfigBuffer[config][subConfig]) {
+                // make new div for config option
+                const keyDiv = document.createElement('div');
+                keyDiv.classList.add('key-option', 'field');
+                // make new label for config option
+                const keyLabel = document.createElement('label');
+                keyLabel.innerHTML = key;
+                keyLabel.classList.add('label');
+                const helpLabel = document.createElement('p');
+                helpLabel.innerHTML =
+                    parsedConfigBuffer[config][subConfig][key]['comment'];
+                helpLabel.classList.add('help');
+
+                const keyControl = document.createElement('div');
+                keyControl.classList.add('control');
+                // make new input for config option
+                const keyInput = document.createElement('input');
+                if (
+                    parsedConfigBuffer[config][subConfig][key]['value'] === true
+                ) {
+                    keyInput.setAttribute('type', 'checkbox');
+                    keyInput.checked = true;
+                    keyInput.classList.add('checkbox');
+                } else if (
+                    parsedConfigBuffer[config][subConfig][key]['value'] ===
+                    false
+                ) {
+                    keyInput.setAttribute('type', 'checkbox');
+                    keyInput.checked = false;
+                    keyInput.classList.add('checkbox');
+                } else {
+                    keyInput.setAttribute('type', 'text');
+                    keyInput.value =
+                        parsedConfigBuffer[config][subConfig][key]['value'];
+                    keyInput.classList.add('input');
+                }
+                keyInput.id =
+                    '{' + config + '}{' + subConfig + '}{' + key + '}';
+                keyControl.appendChild(keyInput);
+                keyDiv.appendChild(keyLabel);
+                keyDiv.appendChild(keyControl);
+                keyDiv.appendChild(helpLabel);
+                subConfigDiv.appendChild(keyDiv);
+            }
+            configDiv.appendChild(subConfigDiv);
+        }
+        document.getElementById('config-options').appendChild(configDiv);
+    }
+    const saveConfigButton = document.createElement('button');
+    saveConfigButton.classList.add('button', 'is-link');
+    saveConfigButton.innerHTML = 'Save Config';
+    saveConfigButton.addEventListener('click', () => {
+        saveConfig();
+    });
+    document.getElementById('config-options').appendChild(saveConfigButton);
+    document.getElementById('config-options').classList.remove('hidden');
 };
 
 const configButtonSetup = () => {
@@ -66,7 +304,8 @@ const configButtonSetup = () => {
     newConfigButton.addEventListener('click', () => {
         // fetch config from github
         fetch(
-            'https://raw.githubusercontent.com/EricAndrechek/trackuino-v2/main/software/config.h'
+            'https://raw.githubusercontent.com/EricAndrechek/trackuino-v2/main/software/config.h?rnd=' +
+                Date.now()
         )
             .then((response) => response.text())
             .then((text) => {
@@ -131,13 +370,24 @@ const parseConfig = (configText, isSchema) => {
             let line = configText[i];
             if (line.startsWith('#define') && lastConfig && lastSubConfig) {
                 // this is a value we need to parse
-                let lineSplit = line.split(' ');
+                let lineSplit = line.split(/\s+/);
                 let key = lineSplit[1];
-                let value = lineSplit[2] || true; // if no value, set to true
+                let value =
+                    lineSplit
+                        .slice(2)
+                        .join(' ')
+                        .replace(/["']/g, '')
+                        .split('//')[0] || true; // if no value, set to true
+                if (value instanceof String && value.startsWith('//'))
+                    value = true;
                 if (debug) console.log(key, value);
-                stagingBuffer[lastConfig][lastSubConfig][key] = value;
+                stagingBuffer[lastConfig][lastSubConfig][key] = {
+                    value: value,
+                    comment: '',
+                };
             } else if (line.startsWith('/')) {
-                line = line.substring(3);
+                line = line.substring(2);
+                if (line.startsWith(' ')) line = line.substring(1);
                 // check if line is a config header
                 if (line in stagingBuffer) {
                     lastConfig = line;
@@ -152,10 +402,23 @@ const parseConfig = (configText, isSchema) => {
                             console.log(`found sub config: ${lastSubConfig}`);
                     } else if (line.startsWith('#define')) {
                         // this is a boolean value that is disabled
-                        let lineSplit = line.split(' ');
+                        let lineSplit = line.split(/\s+/);
                         let key = lineSplit[1];
-                        if (debug) console.log(`found disabled config: ${key}`);
-                        stagingBuffer[lastConfig][lastSubConfig][key] = false;
+                        let value =
+                            lineSplit
+                                .slice(2)
+                                .join(' ')
+                                .replace(/["']/g, '')
+                                .split('//')[0] || undefined; // if no value, set to false
+                        if (value === undefined || value.startsWith('//')) {
+                            // we only want to mark it as a boolean variable if it is not just a commented out parameter
+                            if (debug)
+                                console.log(`found disabled config: ${key}`);
+                            stagingBuffer[lastConfig][lastSubConfig][key] = {
+                                value: false,
+                                comment: '',
+                            };
+                        }
                     }
                 }
             }
